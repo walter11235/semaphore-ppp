aed35d050ff495e4c77ba8f5f196f25818d4bf22
walter11235 <42909488+walter11235@users.noreply.github.com>
2022-10-07 14:41:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# semaphore-ppp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-1gg1aq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component, VERSION } from '@angular/core';
+import {
+  BehaviorSubject,
+  combineLatest,
+  interval,
+  Observable,
+  of,
+  timer,
+} from 'rxjs';
+import { map, takeUntil, withLatestFrom, exhaustMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public sourceInterval = interval(1000).pipe(map((data) => `Source: ${data}`));
+
+  public mySemaphore = new BehaviorSubject(false);
+
+  public observableBuffer;
+
+  constructor() {
+    this.sourceInterval
+      .pipe(
+        withLatestFrom(this.mySemaphore),
+        exhaustMap(([sourceIntervalEmission, semaphoreEmission]) =>
+          semaphoreEmission
+            ? new Observable<void>((subscriber) => {
+                console.log('Panel is opening...');
+                this.observableBuffer = subscriber;
+              })
+            : of(sourceIntervalEmission)
+        )
+      )
+      .subscribe(console.log);
+
+    // this.sourceInterval
+    //   .pipe(
+    //     withLatestFrom(this.mySemaphore),
+    //     filter(([, semaphoreEmission]) => !semaphoreEmission),
+    //     map(([value]) => value)
+    //   )
+    //   .subscribe(console.log);
+  }
+
+  public interruptEpic() {
+    this.mySemaphore.next(true);
+  }
+
+  public continueEpic() {
+    this.mySemaphore.next(false);
+    this.observableBuffer.complete();
+  }
+}
